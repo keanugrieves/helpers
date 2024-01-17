7ccddbf03fbb1aa2913c5637a64fb600d9d7503a
keanugrieves <51007475+keanugrieves@users.noreply.github.com>
2024-01-16 21:13:38 -0800

    Create sort-dates.ts

---
+++ packages/dayjs-helpers/src/utils/sort-dates.ts
@@ -0,0 +1,11 @@
+import type dayjs from "dayjs";
+
+import { NullishDayjsLike } from "../types";
+
+export const _sortDates =
+  (_dayjs: typeof dayjs) => (values: NullishDayjsLike[]) =>
+    values
+      .flatMap((v) => (v && _dayjs(v).isValid() ? v : []))
+      .sort((a, b) =>
+        _dayjs(a).toISOString() > _dayjs(b).toISOString() ? -1 : 1
+      );
