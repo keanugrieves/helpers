09382f38b3f60b49b22aad5e698c9bdf3b56b605
keanugrieves <51007475+keanugrieves@users.noreply.github.com>
2024-01-16 21:51:28 -0800

    Create .eslintrc.cjs

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+// This configuration only applies to the package manager root.
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  ignorePatterns: ["apps/**", "packages/**", "tools/**"],
+  extends: ["@repo/eslint-config/library.cjs"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+  },
+};
